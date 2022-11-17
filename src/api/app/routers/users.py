91e8d22b42819d9ef291bf1a411ab9e0eb55a33c
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-11-17 14:38:36 +0400

    Display is admin in user serializator.

--- src/api/app/routers/users.py
+++ src/api/app/routers/users.py
@@ -21,7 +21,7 @@ async def method_users_me(req: Request, show_courses: bool = False, db: Session 
     """Returns id, email for current user."""
     auth_data = query_auth_data_from_request(req, db)
     
-    serialized_user = serialize_user(auth_data.user)
+    serialized_user = serialize_user(auth_data.user, allow_display_is_admin=True)
     if show_courses:
         purchased_courses = crud.user_course.get_by_user_id(db, user_id=auth_data.user_id)
         return api_success(serialized_user | serialize_user_courses(purchased_courses))
@@ -40,7 +40,7 @@ async def method_users_me_courses(req: Request, db: Session = Depends(get_db)) -
 
 
 @router.get("/users/list")
-async def method_users_list(req: Request, db: Session = Depends(get_db)) -> JSONResponse:
+async def method_users_list(req: Request, show_is_admins: bool = False, db: Session = Depends(get_db)) -> JSONResponse:
     """Returns list of all users (Permitted only)."""
 
     auth_data = query_auth_data_from_request(req, db)
@@ -50,4 +50,4 @@ async def method_users_list(req: Request, db: Session = Depends(get_db)) -> JSON
     users = crud.user.get_all(db)
     return api_success({
         "total": len(users)
-    } | serialize_users(users))
+    } | serialize_users(users, allow_display_is_admins=show_is_admins))
--- src/api/app/serializers/user.py
+++ src/api/app/serializers/user.py
@@ -6,7 +6,7 @@
 from app.database.models.user import User
 
 
-def serialize(user: User, in_list: bool = False):
+def serialize(user: User, allow_display_is_admin: bool = False, in_list: bool = False):
     """Returns dict object for API response with serialized user data."""
 
     serialized_user = {
@@ -14,17 +14,29 @@ def serialize(user: User, in_list: bool = False):
         "email": user.email
     }
 
+    if allow_display_is_admin and user.is_admin:
+        serialized_user |= {
+            "is_admin": user.is_admin
+        }
+    
     if in_list:
         return serialized_user
 
     return {"user": serialized_user}
 
 
-def serialize_list(users: list[User]) -> dict:
+def serialize_list(users: list[User], allow_display_is_admins: bool = False) -> dict:
     """Returns dict object for API response with serialized users list data."""
 
     return {
-        "users": [serialize(user=user, in_list=True) for user in users]
+        "users": [
+            serialize(
+                user=user, 
+                allow_display_is_admin=allow_display_is_admins, 
+                in_list=True
+            ) 
+            for user in users
+        ]
     }
 
 
